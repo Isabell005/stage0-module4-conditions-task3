9d666853439d1bd115ce9949ad7ad5b7c55f8dde
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-14 14:02:57 +0400

    stage0Module4ConditionsTask3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'y') {
+            System.out.println("Vowel");
+        } else if (character == 'q' || character == 'w' || character == 'r' || character == 't' || character == 'p' || character == 's'||character == 'd' || character == 'f' || character == 'g' || character == 'h' || character == 'j' || character == 'k'||character == 'l' || character == 'z' || character == 'x' || character == 'c' || character == 'v' || character == 'b'|| character == 'n' || character == 'm') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+
+        if ((symbol >= 0041) || (symbol <= 005) && (symbol>=0061 || symbol<=007)) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
